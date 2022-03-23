74a344b7fe34c3eaf2468f53124fa47f8567c355
mauriciosm13 <70268675+mauriciosm13@users.noreply.github.com>
2022-03-23 12:44:41 -0300

    Commit
    Delete

--- api/rotas/fornecedores/produtos/Produto.js
+++ api/rotas/fornecedores/produtos/Produto.js
@@ -1,3 +1,4 @@
+const { type } = require('express/lib/response')
 const Tabela = require('./TabelaProduto')
 class Produto {
     constructor({ id, titulo, preco, estoque, fornecedor, dataCriacao, dataAtualizacao, versao }) {
@@ -10,7 +11,18 @@ class Produto {
         this.dataAtualizacao = dataAtualizacao
         this.versao = versao
     }
+
+    validar() {
+        if (typeof this.titulo !== 'string' || this.titulo.length === 0) {
+            throw new Error('O campo titulo está inválido')
+        }
+        if (typeof this.preco !== 'number' || this.preco === 0) {
+            throw new Error('O campo preço está invalido')
+        }
+
+    }
     async criar() {
+        this.validar()
         const resultado = await Tabela.inserir({
             titulo: this.titulo,
             preco: this.preco,
@@ -21,6 +33,9 @@ class Produto {
         this.dataCriacao = resultado.dataCriacao
         this.dataAtualizacao = resultado.dataCriacao
         this.versao = resultado.versao
+    }
+    apagar() {
+        return Tabela.remover(this.id, this.fornecedor)
     }
 }
 
